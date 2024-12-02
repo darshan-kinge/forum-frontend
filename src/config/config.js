7c466e95f7696b43f4f831e5fc4b6461ff11dc7a
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2024-12-02 23:52:23 +0530

    config changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     port: 3000 ,
     serverUrl: import.meta.env.SERVER_URL,
-    apiVersion: 'v1' || import.meta.env.API_VERSION,
+    apiVersion: import.meta.env.API_VERSION,
 }
 
 export default config;

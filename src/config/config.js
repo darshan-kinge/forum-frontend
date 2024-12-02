9a8c91dbfe42237c2b828f91ca4afaeb8bbe7f1a
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2024-12-02 21:01:54 +0530

    updated the config again

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     port: 3000 ,
-    serverUrl: `http://${import.meta.env.SERVER_URL}` || 'http://localhost:3000',
+    serverUrl: `${import.meta.env.SERVER_URL}` || 'http://localhost:3000',
     clientUrl: 'http://localhost:5173' || '',
     apiVersion: 'v1' || '',
 }

abbc14d3c14c835386741c4a294a2fca6a83720e
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2024-12-07 13:47:19 +0530

    server url fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
+
 const config = {
     port: 3000,
-    serverUrl: 'http://localhost:3000' || import.meta.env.SERVER_URL || 'https://forum-backend-6zul.onrender.com',
+    serverUrl: import.meta.env.SERVER_URL || 'https://forum-backend-6zul.onrender.com',
     apiVersion: import.meta.env.API_VERSION || 'v1',
 }
 

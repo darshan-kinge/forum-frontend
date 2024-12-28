38ec9b0916acc00fe4611d6f5323102ffb04dc58
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2024-12-28 08:07:25 +0530

    server url changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     port: 3000,
-    serverUrl: 'http://localhost:3000',
+    serverUrl: 'https://forum-backend-6zul.onrender.com',
     apiVersion: import.meta.env.API_VERSION || 'v1',
 }
 

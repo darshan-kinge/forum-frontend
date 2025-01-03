c405a66d8e865af97bbebee865cebe89290f9fb1
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2025-01-03 20:49:49 +0530

    server url1

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     port: 3000,
-    serverUrl: process.env.VITE_SERVER_URL, // 'https://forum-backend-6zul.onrender.com',
+    serverUrl: import.meta.env.VITE_SERVER_URL, // 'https://forum-backend-6zul.onrender.com',
     apiVersion: 'v1',
 }
 

555d6929c67745d3141211817558973fc97c81be
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2025-01-03 20:47:39 +0530

    server url1

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     port: 3000,
-    serverUrl: process.env.SERVER_URL, // 'https://forum-backend-6zul.onrender.com',
+    serverUrl: process.env.VITE_SERVER_URL, // 'https://forum-backend-6zul.onrender.com',
     apiVersion: 'v1',
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import config from './src/config/config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: config.serverUrl,
+        changeOrigin: true,
+      },
+    },
+  },
 })

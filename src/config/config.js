68c55f9782f412a505885074a393c7436473ebdd
Darshan <itsmedarshan8@gmail.com>
2025-11-06 17:57:12 +0530

    server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     port: 5000,
-    serverUrl: import.meta.env.VITE_SERVER_URL || 'https://server.snsf.live',
+    serverUrl: import.meta.env.VITE_SERVER_URL || 'https://api.snsf.live',
     apiVersion: 'v1',
     youtubeApiKey: 'AIzaSyB-HwydUQt2b55fG1OI_U6lOAunVUHzzIc',
     playlistId: "PLMsru-lJybKRm_tOl9CN50hOMY1u0Wkev"

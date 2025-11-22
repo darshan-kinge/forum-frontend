bc61a60be59fa587d13359a1be010e979dacb856
Darshan <itsmedarshan8@gmail.com>
2025-11-23 00:59:44 +0530

    admin control frontend

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_SERVER_URL || 'https://server.snsf.live',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
   define: {
     'process.env': process.env
   }
